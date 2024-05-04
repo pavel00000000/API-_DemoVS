2f2c3002ba41e980db74dc2ed0969180d56e7364
roboty.p.d <freddik1990@gmail.com>
2024-05-04 12:35:25 +0300

    gnfgh

--- obj/Debug/net6.0/API _Demo.AssemblyInfo.cs
+++ obj/Debug/net6.0/API _Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API _Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566e18fd1cc48fa162242aeae56358a4fc71a522")]
 [assembly: System.Reflection.AssemblyProductAttribute("API _Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API _Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
